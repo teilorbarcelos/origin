9353ec85b779aa3935ff48c71dc3ca8172705a3a
Teilor Souza Barcelos <tsb-@live.com>
2021-09-26 12:16:27 -0300

    fontsource added, variables created

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,10 +5,10 @@ import globals from '../../styles/globals.module.css'
 
 export default function Header() {
   return (
-    <header className={`${styles.container} ${globals.container}`}>
-      <nav className={styles.nav}>
+    <header className={styles.header}>
+      <nav className={globals.container}>
         <Link href={'#'}>
-          <a className={styles.logo}>beautysalon.</a>
+          <a className={`${styles.logo}`}>beautysalon.</a>
         </Link>
         <div className={styles.menu}>...</div>
       </nav>
